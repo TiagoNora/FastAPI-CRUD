6d67cc67828b1c977c4a45b09ea5dce234bb6a35
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-12-15 03:48:32 +0000

    feat: Criação da tabela na base de dados

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from routes import checkRouter
+from sqlmodel import SQLModel
+from models import *
+from database import db
+
+SQLModel.metadata.create_all(db.engine)
 
 app = FastAPI()
 
 app.include_router(checkRouter.router)
-
