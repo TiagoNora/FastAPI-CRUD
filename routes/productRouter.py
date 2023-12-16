875cc433785248366cff72b0121d31d49e7f55e9
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-12-16 22:31:40 +0000

    feat: Added the endpoints to the product router

---
+++ routes/productRouter.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from models import *
+from database import *
+from typing import List
+
+router = APIRouter(prefix='/products', tags=['PRODUCTS'])
+
+@router.post("/", response_model=ProductRead)
+def createProduct(*, session: Session = Depends(get_session), product: ProductCreate):
+    db_product = Product.validate(product)
+    session.add(db_product)
+    session.commit()
+    session.refresh(db_product)
+    return db_product
+
+@router.get("/", response_model=List[ProductRead])
+def getProducts(*, session: Session = Depends(get_session)):
+    products = session.exec(select(Product)).all()
+    return products
+
+@router.get("/{product_id}", response_model=ProductRead)
+def getProductByID(*, session: Session = Depends(get_session), product_id: int):
+    product = session.get(Product, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@router.delete("/{product_id}")
+def deleteProductByID(*, session: Session = Depends(get_session), product_id: int):
+    product = session.get(Product, product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    session.delete(product)
+    session.commit()
+    return {"ok": True}
+
+
