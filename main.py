16f923362eb840027b11d0c018811de5c69d2cef
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-12-16 22:29:21 +0000

    feat: Added the product router to the app

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import checkRouter
+from routes import checkRouter, productRouter
 from sqlmodel import SQLModel
 from models import *
 from database import db
@@ -9,3 +9,4 @@ SQLModel.metadata.create_all(db.engine)
 app = FastAPI()
 
 app.include_router(checkRouter.router)
+app.include_router(productRouter.router)
