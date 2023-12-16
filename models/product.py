fc348406a5b17e70761526a43c68dd518bd7f70c
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2023-12-16 22:30:50 +0000

    feat: Updated the product model and created schemas for the model

--- models/product.py
+++ models/product.py
@@ -4,9 +4,27 @@ from datetime import datetime
 
 class Product(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    sku: str
     description: str
     designation: str
     company: str
     price: float
     created_at: Optional[datetime] = Field(default=datetime.utcnow(), nullable=False)
-    last_edited: Optional[datetime] = Field(default_factory=datetime.utcnow(), nullable=False)
+    last_edited: Optional[datetime] = Field(default_factory=datetime.utcnow, nullable=False)
+
+class ProductCreate(SQLModel):
+    sku: str
+    description: str
+    designation: str
+    company: str
+    price: float
+
+class ProductRead(SQLModel):
+    id: int
+    sku: str
+    description: str
+    designation: str
+    company: str
+    price: float
+    created_at: datetime
+    last_edited: datetime
